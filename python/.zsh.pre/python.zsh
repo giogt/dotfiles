eb1d48ca6764596776dc9e071cb6e1964b01580d
giogt <giogt.dev@pm.me>
2026-02-08 23:08:51 +0000

    Changed comment

--- python/.zsh.pre/python.zsh
+++ python/.zsh.pre/python.zsh
@@ -1,4 +1,4 @@
-# needed for Poetry (Python package manager)
+# Poetry (Python package manager) binary default location is ~/.local/bin
 if [[ -d "$HOME/.local/bin" ]]; then
   export PATH="$HOME/.local/bin:$PATH"
 fi
