f32bbe54942c984350e3b16dbc2a3122e56bee41
giogt <giogt.dev@pm.me>
2026-02-25 22:14:31 +0000

    Add Raycast script to launch Leo (Brave AI)

---
+++ scripts/.scripts/raycast/leo-ai.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Leo AI
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ../icons/brave/product-brave-leo.svg 
+# @raycast.packageName AI
+
+# Documentation:
+# @raycast.description Brave Leo AI 
+# @raycast.author giogt
+
+open -a /Applications/Brave\ Browser.app "brave://leo-ai"
