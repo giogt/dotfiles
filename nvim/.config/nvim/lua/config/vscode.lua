55c0278d3aa3678ec28bad273ef2c8d7a2b5b159
giogt <giogt.dev@pm.me>
2026-02-26 07:31:57 +0000

    Change VS Code Neovim plugin config

--- nvim/.config/nvim/lua/config/vscode.lua
+++ nvim/.config/nvim/lua/config/vscode.lua
@@ -1,3 +1,1 @@
--- vim.g.mapleader = ' '
-vim.keymap.set({'n', 'o', 'v'}, 'H', '^', { remap=false })
-vim.keymap.set({'n', 'o', 'v'}, 'L', '$', { remap=false })
+-- [[ VS Code Neovim plugin config ]]
