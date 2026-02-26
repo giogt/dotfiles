b924e10d335cfdfc3250c9f091ca0314b1c8be74
giogt <giogt.dev@pm.me>
2026-02-26 18:13:10 +0000

    Add nvim tab global options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -66,3 +66,9 @@ vim.o.scrolloff = 10
 -- raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- Tabs global options
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
