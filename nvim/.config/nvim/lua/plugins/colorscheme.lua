6ef79df0c65989bb081f3a3960c3417a9f7e254e
giogt <giogt.dev@pm.me>
2026-02-26 18:15:27 +0000

    Change nvim colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,26 +5,24 @@
 return {
 
 	-- Tokionight
-	"folke/tokyonight.nvim",
-	priority = 1000, -- Load before all other start plugins
-	config = function()
-		---@diagnostic disable-next-line: missing-fields
-		require("tokyonight").setup({
-			styles = {
-				comments = { italic = false }, -- Disable italics in comments
-			},
-		})
-		vim.cmd.colorscheme("tokyonight-night")
-	end,
+	-- "folke/tokyonight.nvim",
+	-- priority = 1000, -- Load before all other start plugins
+	-- config = function()
+	-- 	---@diagnostic disable-next-line: missing-fields
+	-- 	require("tokyonight").setup({
+	-- 		styles = {
+	-- 			comments = { italic = false }, -- Disable italics in comments
+	-- 		},
+	-- 	})
+	-- 	vim.cmd.colorscheme("tokyonight-night")
+	-- end,
 
 	-- Nightfox
-	-- {
-	-- 	"EdenEast/nightfox.nvim",
-	-- 	priority = 1000, -- Load before all other start plugins
-	-- },
-	-- config = function()
-	-- 	--  dark: duskfox
-	-- 	--  light: dayfox
-	-- 	vim.cmd.colorscheme("duskfox")
-	-- end,
+	"EdenEast/nightfox.nvim",
+	priority = 1000, -- Load before all other start plugins
+	config = function()
+		--  dark: duskfox
+		--  light: dayfox
+		vim.cmd.colorscheme("duskfox")
+	end,
 }
