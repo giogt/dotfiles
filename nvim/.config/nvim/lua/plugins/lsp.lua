0480578fdcdd438af094e56fa130401fc9b03e22
giogt <giogt.dev@pm.me>
2026-02-27 01:22:28 +0000

    Change nvim keymaps and improved config comments

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -4,84 +4,38 @@ return { -- LSP Plugins
 	-- Main LSP Configuration
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		-- Automatically install LSPs and related tools to stdpath for Neovim
-		-- Mason must be loaded before its dependents so we need to set it up here.
-		-- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+		-- Note: `opts = {}` is the same as calling `require('mason').setup({})`
 		{ "mason-org/mason.nvim", opts = {} },
-
-		-- This plugin streamlines Neovim's LSP setup by automating server installation and
-		-- activation, providing helpful management commands, and mapping mason.nvim packages to
-		-- nvim-lspconfig configurations.
 		{ "mason-org/mason-lspconfig.nvim", opts = {} },
-
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-		-- Useful status updates for LSP.
+		-- Status updates for LSP.
 		{ "j-hui/fidget.nvim", opts = {} },
 
 		-- Allows extra capabilities provided by blink.cmp
 		"saghen/blink.cmp",
 	},
 	config = function()
-		-- Brief aside: **What is LSP?**
-		--
-		-- LSP is an initialism you've probably heard, but might not understand what it is.
-		--
-		-- LSP stands for Language Server Protocol. It's a protocol that helps editors
-		-- and language tooling communicate in a standardized fashion.
-		--
-		-- In general, you have a "server" which is some tool built to understand a particular
-		-- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-		-- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-		-- processes that communicate with some "client" - in this case, Neovim!
-		--
-		-- LSP provides Neovim with features like:
-		--  - Go to definition
-		--  - Find references
-		--  - Autocompletion
-		--  - Symbol Search
-		--  - and more!
-		--
-		-- Thus, Language Servers are external tools that must be installed separately from
-		-- Neovim. This is where `mason` and related plugins come into play.
-		--
-		-- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-		-- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-		--  This function gets run when an LSP attaches to a particular buffer.
-		--    That is to say, every time a new file is opened that is associated with
-		--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-		--    function will be executed to configure the current buffer
+		--  This function runs each time a LSP attaches to a buffer
+		--   (e.g., when opening `main.rs`, which is associated with `rust_analyzer`)
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 			callback = function(event)
-				-- NOTE: Remember that Lua is a real programming language, and as such it is possible
-				-- to define small helper and utility functions so you don't have to repeat yourself.
-				--
-				-- In this case, we create a function that lets us more easily define mappings specific
-				-- for LSP related items. It sets the mode, buffer and description for us each time.
 				local map = function(keys, func, desc, mode)
 					mode = mode or "n"
 					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
 
-				-- Rename the variable under your cursor.
-				--  Most Language Servers support renaming across files, etc.
-				map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+				map("grn", vim.lsp.buf.rename, "Rename")
+				map("gra", vim.lsp.buf.code_action, "Code action", { "n", "x" })
 
-				-- Execute a code action, usually your cursor needs to be on top of an error
-				-- or a suggestion from your LSP for this to activate.
-				map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+				-- Goto declaration (e.g., header file in C)
+				--  Not to be confused with Goto definition
+				map("grD", vim.lsp.buf.declaration, "Goto declaration")
 
-				-- WARN: This is not Goto Definition, this is Goto Declaration.
-				--  For example, in C this would take you to the header.
-				map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-				-- The following two autocommands are used to highlight references of the
-				-- word under your cursor when your cursor rests there for a little while.
-				--    See `:help CursorHold` for information about when this is executed
-				--
-				-- When you move your cursor, the highlights will be cleared (the second autocommand).
+				-- Highlight references of the word under the cursor after a little while.
+				-- Moving the cursor will clear the highlight.
+				--    See `:help CursorHold` for details
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
 				if client and client:supports_method("textDocument/documentHighlight", event.buf) then
 					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
@@ -106,52 +60,41 @@ return { -- LSP Plugins
 					})
 				end
 
-				-- The following code creates a keymap to toggle inlay hints in your
-				-- code, if the language server you are using supports them
-				--
-				-- This may be unwanted, since they displace some of your code
 				if client and client:supports_method("textDocument/inlayHint", event.buf) then
 					map("<leader>th", function()
 						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-					end, "[T]oggle Inlay [H]ints")
+					end, "Toggle inlay hints")
 				end
 			end,
 		})
 
-		-- LSP servers and clients are able to communicate to each other what features they support.
-		--  By default, Neovim doesn't support everything that is in the LSP specification.
-		--  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-		--  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+		-- Neovim doesn't support all LSP capabilities out of the box.
+		-- Plugins such as blink.cmp support additional capabilities. When using this kind of
+		-- plugins, the additional capabilities need to be broadcasted to the LSP server.
+
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-		-- Enable the following language servers
-		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+		--  Add/remove any required LSP here. They will automatically be installed.
 		--  See `:help lsp-config` for information about keys and how to configure
+		--
+		--  NOTE: for some languages, consider using a specific plugin instead of adding a
+		--  LSP here. For example:
+		--  - https://github.com/mrcjkb/rustaceanvim instead of the `rust_analyzer` LSP
+		--  - https://github.com/pmizio/typescript-tools.nvim instead of the `ts_ls` LSP
+		--
 		local servers = {
 			-- clangd = {},
 			-- gopls = {},
 			-- pyright = {},
 			-- rust_analyzer = {},
-			--
-			-- Some languages (like typescript) have entire language plugins that can be useful:
-			--    https://github.com/pmizio/typescript-tools.nvim
-			--
-			-- But for many setups, the LSP (`ts_ls`) will work just fine
-			-- ts_ls = {},
 		}
 
-		-- Ensure the servers and tools above are installed
-		--
-		-- To check the current status of installed tools and/or manually install
-		-- other tools, you can run
-		--    :Mason
-		--
-		-- You can press `g?` for help in this menu.
+		-- List of tools to install with Mason at startup, if they are not installed yet.
+		-- You can add other tools to the list, and/or install additional tools with :Mason
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"lua_ls", -- Lua Language server
 			"stylua", -- Used to format Lua code
-			-- You can add other tools here that you want Mason to install
 		})
 
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,56 +1,33 @@
 -- [[ Telescope ]]
 -- Fuzzy Finder (files, lsp, etc)
 
+-- Usage:
+--  :Telescope <command>
+--
+--  Or use the keymaps defined below.
+
 return {
 	"nvim-telescope/telescope.nvim",
 	enabled = true,
 	event = "VimEnter",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+		{
 			"nvim-telescope/telescope-fzf-native.nvim",
 
 			-- `build` is used to run some command when the plugin is installed/updated.
-			-- This is only run then, not every time Neovim starts up.
 			build = "make",
 
-			-- `cond` is a condition used to determine whether this plugin should be
-			-- installed and loaded.
+			-- `cond` determines whether this plugin should be installed and loaded
 			cond = function()
 				return vim.fn.executable("make") == 1
 			end,
 		},
 		{ "nvim-telescope/telescope-ui-select.nvim" },
-
-		-- Useful for getting pretty icons, but requires a Nerd Font.
 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
 	config = function()
-		-- Telescope is a fuzzy finder that comes with a lot of different things that
-		-- it can fuzzy find! It's more than just a "file finder", it can search
-		-- many different aspects of Neovim, your workspace, LSP, and more!
-		--
-		-- The easiest way to use Telescope, is to start by doing something like:
-		--  :Telescope help_tags
-		--
-		-- After running this command, a window will open up and you're able to
-		-- type in the prompt window. You'll see a list of `help_tags` options and
-		-- a corresponding preview of the help.
-		--
-		-- Two important keymaps to use while in Telescope are:
-		--  - Insert mode: <c-/>
-		--  - Normal mode: ?
-		--
-		-- This opens a window that shows you all of the keymaps for the current
-		-- Telescope picker. This is really useful to discover what Telescope can
-		-- do as well as how to actually do it!
-
-		-- [[ Configure Telescope ]]
-		-- See `:help telescope` and `:help telescope.setup()`
 		require("telescope").setup({
-			-- You can put your default mappings / updates / etc. in here
-			--  All the info you're looking for is in `:help telescope.setup()`
-			--
 			-- defaults = {
 			--   mappings = {
 			--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
@@ -66,24 +43,46 @@ return {
 		pcall(require("telescope").load_extension, "fzf")
 		pcall(require("telescope").load_extension, "ui-select")
 
-		-- See `:help telescope.builtin`
 		local builtin = require("telescope.builtin")
-		-- Override find_files to always show hidden files
-		local find_files = function()
-			builtin.find_files({ hidden = true, no_ignore = false, follow = true })
-		end
 
-		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>sf", find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set({ "n", "v" }, "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		vim.keymap.set("n", "<leader>sc", builtin.commands, { desc = "[S]earch [C]ommands" })
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+		-- [[ Keymaps ]]
+
+		-- Open
+		vim.keymap.set("n", "<leader>o", function()
+			builtin.find_files({ hidden = true, no_ignore = false, follow = true })
+		end, { desc = "Open file" })
+		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Open buffer" })
+
+		-- Search
+		vim.keymap.set("n", "<leader>/b", function()
+			builtin.live_grep({
+				grep_open_files = true,
+				prompt_title = "Live grep in buffers",
+			})
+		end, { desc = "Search in buffers" })
+		vim.keymap.set("n", "<leader>//", function()
+			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+				winblend = 10,
+				previewer = false,
+			}))
+		end, { desc = "Search in current file [/]" })
+
+		vim.keymap.set("n", "<leader>/f", builtin.live_grep, { desc = "Search in files" })
+		vim.keymap.set("n", "<leader>/r", builtin.oldfiles, { desc = "Search recent files" })
+		vim.keymap.set({ "n", "v" }, "<leader>/w", builtin.grep_string, { desc = "Search current word" })
+		vim.keymap.set("n", "<leader>/d", builtin.resume, { desc = "Search dialog (resume last)" })
+
+		-- Diagnostics
+		vim.keymap.set("n", "<leader>ds", builtin.diagnostics, { desc = "Search diagnostics" })
+
+		-- Neovim
+		vim.keymap.set("n", "<leader>nc", builtin.commands, { desc = "Search nvim commands" })
+		vim.keymap.set("n", "<leader>nh", builtin.help_tags, { desc = "Search nvim help" })
+		vim.keymap.set("n", "<leader>nk", builtin.keymaps, { desc = "Search nvim keymaps" })
+		vim.keymap.set("n", "<leader>no", function()
+			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+		end, { desc = "Open nvim config file" })
+		vim.keymap.set("n", "<leader>nt", builtin.builtin, { desc = "Search telescope commands" })
 
 		-- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
 		-- it is better explained there). This allows easily switching between pickers if you prefer using something else!
@@ -92,74 +91,32 @@ return {
 			callback = function(event)
 				local buf = event.buf
 
-				-- Find references for the word under your cursor.
-				vim.keymap.set("n", "grr", builtin.lsp_references, { buffer = buf, desc = "[G]oto [R]eferences" })
+				vim.keymap.set("n", "grr", builtin.lsp_references, { buffer = buf, desc = "Goto references" })
+				vim.keymap.set("n", "gri", builtin.lsp_implementations, { buffer = buf, desc = "Goto implementation" })
+				vim.keymap.set("n", "grd", builtin.lsp_definitions, { buffer = buf, desc = "Goto definition" })
 
-				-- Jump to the implementation of the word under your cursor.
-				-- Useful when your language has ways of declaring types without an actual implementation.
-				vim.keymap.set(
-					"n",
-					"gri",
-					builtin.lsp_implementations,
-					{ buffer = buf, desc = "[G]oto [I]mplementation" }
-				)
-
-				-- Jump to the definition of the word under your cursor.
-				-- This is where a variable was first declared, or where a function is defined, etc.
-				-- To jump back, press <C-t>.
-				vim.keymap.set("n", "grd", builtin.lsp_definitions, { buffer = buf, desc = "[G]oto [D]efinition" })
-
-				-- Fuzzy find all the symbols in your current document.
-				-- Symbols are things like variables, functions, types, etc.
+				-- Fuzzy find symbols (e.g., variables, functions, types, etc.)
 				vim.keymap.set(
 					"n",
 					"gO",
 					builtin.lsp_document_symbols,
-					{ buffer = buf, desc = "Open Document Symbols" }
+					{ buffer = buf, desc = "Open document symbols" }
 				)
-
-				-- Fuzzy find all the symbols in your current workspace.
-				-- Similar to document symbols, except searches over your entire project.
 				vim.keymap.set(
 					"n",
 					"gW",
 					builtin.lsp_dynamic_workspace_symbols,
-					{ buffer = buf, desc = "Open Workspace Symbols" }
+					{ buffer = buf, desc = "Open workspace symbols" }
 				)
 
-				-- Jump to the type of the word under your cursor.
-				-- Useful when you're not sure what type a variable is and you want to see
-				-- the definition of its *type*, not where it was *defined*.
+				-- Type
 				vim.keymap.set(
 					"n",
 					"grt",
 					builtin.lsp_type_definitions,
-					{ buffer = buf, desc = "[G]oto [T]ype Definition" }
+					{ buffer = buf, desc = "Goto type definition" }
 				)
 			end,
 		})
-
-		-- Override default behavior and theme when searching
-		vim.keymap.set("n", "<leader>/", function()
-			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
-			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
-				previewer = false,
-			}))
-		end, { desc = "[/] Fuzzily search in current buffer" })
-
-		-- It's also possible to pass additional configuration options.
-		--  See `:help telescope.builtin.live_grep()` for information about particular keys
-		vim.keymap.set("n", "<leader>s/", function()
-			builtin.live_grep({
-				grep_open_files = true,
-				prompt_title = "Live Grep in Open Files",
-			})
-		end, { desc = "[S]earch [/] in Open Files" })
-
-		-- Shortcut for searching your Neovim configuration files
-		vim.keymap.set("n", "<leader>sn", function()
-			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[S]earch [N]eovim files" })
 	end,
 }
