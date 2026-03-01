b2fd44a7c9db30ae61a37ee0fcd61eec57238b46
giogt <giogt.dev@pm.me>
2026-03-01 06:39:07 +0000

    Add comment to nvim rust plugin

--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -1,5 +1,9 @@
 -- [[ Rust plugins ]]
 
+-- NOTE: this plugin is meant to work with rust-analyzer installed globally via cargo.
+--  See: https://rust-analyzer.github.io/book/installation.html
+--  If you installed rust-analyzer via Mason, please uninstall it to avoid conflicts.
+
 return {
 	"mrcjkb/rustaceanvim",
 	version = "^7", -- Recommended
