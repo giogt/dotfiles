a156914a8a91c09f5c118ff8e35ce63bed58127d
giogt <giogt.dev@pm.me>
2026-03-01 02:48:08 +0000

    Improve vcfg alias (used to edit nvim config)

--- aliases.nvim/.zsh.aliases/aliases.nvim.zsh
+++ aliases.nvim/.zsh.aliases/aliases.nvim.zsh
@@ -1,7 +1,7 @@
 alias v="nvim"
 alias vi="nvim"
 alias vim="nvim"
-alias vcfg="nvim --cmd \"cd ~/.config/nvim\" ~/.config/nvim/init.lua ~/.config/nvim/lua/config/colorscheme.lua ~/.config/nvim/lua/config/giogt.lua"
+alias vcfg="nvim --cmd \"cd ~/.config/nvim\" ~/.config/nvim/init.lua"
 
 export NVIM_CONFIG_COLORSCHEME="${HOME}/.config/nvim/lua/config/colorscheme.lua"
 nvim_colorscheme_light() {
