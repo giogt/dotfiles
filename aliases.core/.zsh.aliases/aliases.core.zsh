d135c87b39385235d29746971fa887bef87a3252
giogt <giogt.dev@pm.me>
2026-03-07 00:46:25 +0000

    Update eza shell aliases

--- aliases.core/.zsh.aliases/aliases.core.zsh
+++ aliases.core/.zsh.aliases/aliases.core.zsh
@@ -1,10 +1,10 @@
 alias ll="ls -l"
 alias la="ls -a"
 alias lla="ls -la"
-alias e="eza -g --git"
-alias el="eza -lg --git"
-alias ea="eza -ag --git"
-alias ela="eza -lag --git"
+alias e="eza -g --group-directories-first --git"
+alias el="eza -lg --group-directories-first --git"
+alias ea="eza -ag --group-directories-first --git"
+alias ela="eza -lag --group-directories-first --git"
 
 alias curl_post_json="curl -X POST -H \"Content-Type: application/json\" -d"
 
