c3e422e3374308c0072ea35744e1853a801751fd
giogt <giogt.dev@pm.me>
2026-03-07 21:54:28 +0000

    Improve eza shell aliases

--- aliases.core/.zsh.aliases/aliases.core.zsh
+++ aliases.core/.zsh.aliases/aliases.core.zsh
@@ -1,10 +1,16 @@
-alias ll="ls -l"
-alias la="ls -a"
-alias lla="ls -la"
-alias e="eza -g --group-directories-first --git"
-alias el="eza -lg --group-directories-first --git"
-alias ea="eza -ag --group-directories-first --git"
-alias ela="eza -lag --group-directories-first --git"
+# eza (ls modern replacement)
+EZA_OPTS="--group-directories-first --icons=auto --git"
+alias e="eza -g ${EZA_OPTS}"
+alias el="eza -lg ${EZA_OPTS}"
+alias ea="eza -ag ${EZA_OPTS}"
+alias ela="eza -lag ${EZA_OPTS}"
+
+# Define aliases commonly used for `ls` to use `eza` instead
+# NOTE: do not redefine `ls`, so that scripts relying on `ls` behaviour still work
+alias l="eza -g ${EZA_OPTS}"
+alias ll="eza -lg ${EZA_OPTS}"
+alias la="eza -ag ${EZA_OPTS}"
+alias lla="eza -lag ${EZA_OPTS}"
 
 alias curl_post_json="curl -X POST -H \"Content-Type: application/json\" -d"
 
