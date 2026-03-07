c1bc4e75c7e2bfccf9e283142f374490cd36d521
giogt <giogt.dev@pm.me>
2026-03-07 21:54:07 +0000

    Add new tmux shell aliases

--- aliases.tmux/.zsh.aliases/aliases.tmux.zsh
+++ aliases.tmux/.zsh.aliases/aliases.tmux.zsh
@@ -1,4 +1,9 @@
 alias ta="tmux attach -t"
+
+# tmux sessions
+alias tl="tmux list-sessions"
 alias tls="tmux list-sessions"
+alias tn="tmux new-session -s"
 alias tns="tmux new-session -s"
+alias tk="tmux kill-session -t"
 alias tks="tmux kill-session -t"
