2fd08bf7941ffc0707b9fa358b0160ae0a490024
giogt <giogt.dev@pm.me>
2026-03-16 02:25:43 +0000

    aliases.nvim functionality improved and moved to nvim and dev

--- dev/.zsh.post/dev.zsh
+++ dev/.zsh.post/dev.zsh
@@ -1,2 +1,13 @@
 alias de="dev-env"
 
+dev-github() {
+  local selected_dir=$(fd . "$HOME/dev/github.com/giogt" -d 1 -a --type directory \
+      --exclude .git --exclude node_modules/ --exclude target/ \
+      | fzf --preview 'ls -l {}' --height=40% --prompt="📁 " \
+  )
+  if [[ -n "${selected_dir}" && -d "${selected_dir}" ]]; then
+    builtin cd "${selected_dir}"
+    dev-env
+  fi
+}
+
---
+++ nvim/.zsh.post/nvim.zsh
@@ -0,0 +1,6 @@
+# nvim aliases
+alias v="nvim"
+alias vi="nvim"
+alias vim="nvim"
+alias vcfg="nvim --cmd \"cd ~/.config/nvim\" ~/.config/nvim/init.lua"
+
