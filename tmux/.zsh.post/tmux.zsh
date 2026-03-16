cd5e3b9b5130220d5dcbf7964a22784a635d2d57
giogt <giogt.dev@pm.me>
2026-03-16 02:28:38 +0000

    Move aliases.tmux under tmux

--- tmux/.zsh.post/tmux.zsh
+++ tmux/.zsh.post/tmux.zsh
@@ -1,9 +1,11 @@
+# tmux aliases - attach
 alias ta="tmux attach -t"
 
-# tmux sessions
+# tmux aliases - sessions
 alias tl="tmux list-sessions"
 alias tls="tmux list-sessions"
 alias tn="tmux new-session -s"
 alias tns="tmux new-session -s"
 alias tk="tmux kill-session -t"
 alias tks="tmux kill-session -t"
+
