0c3d9e24417f1ce0c08c55236a5f6fdb14b2c4a1
giogt <giogt.dev@pm.me>
2026-03-17 21:42:09 +0000

    [scripts] Use Ghostty to launch TUI apps

--- scripts/.scripts/raycast/launch-yazi.sh
+++ scripts/.scripts/raycast/launch-yazi.sh
@@ -14,7 +14,7 @@
 # @raycast.author giogt
 
 # Open with Ghostty
-# open -na /Applications/Ghostty.app/Contents/MacOS/ghostty --args --window-width=160 --window-height=55 --quit-after-last-window-closed=true --command=yazi
+open -na /Applications/Ghostty.app/Contents/MacOS/ghostty --args --window-width=160 --window-height=55 --quit-after-last-window-closed=true --command=yazi
 
 # Open with Alacritty
-open -na /Applications/Alacritty.app/Contents/MacOS/alacritty --args --title="Yazi" --command=yazi
+# open -na /Applications/Alacritty.app/Contents/MacOS/alacritty --args --title="Yazi" --command=yazi
