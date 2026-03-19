2457fd09ff4ef5c00e68a329eff8800285de96a1
giogt <giogt.dev@pm.me>
2026-03-19 03:47:14 +0000

    [nvim] Show current buffer relative path in Lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -38,7 +38,33 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, -- Displays file status (readonly, modified)
+						newfile_status = false, -- True displays status for newly created file
+
+						-- 0: Just filename
+						-- 1: Relative path
+						-- 2: Absolute path
+						-- 3: Absolute path, with tilde as the home directory
+						-- 4: Filename and parent dir, with tilde as the home directory
+						path = 1,
+
+						-- Shortens path to leave 40 spaces in the window
+						-- for other components. (Caution: setting this
+						-- too high may cause the path to disappear on
+						-- small screens)
+						shorting_target = 40,
+
+						symbols = {
+							modified = "[+]", -- Text to show when the file is modified
+							readonly = "[-]", -- Text to show when the file is non-modifiable or readonly
+							unnamed = "[No Name]", -- Text to show for unnamed buffers
+							newfile = "[New]", -- Text to show for nealy created file before first write
+						},
+					},
+				},
 				lualine_x = { "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
