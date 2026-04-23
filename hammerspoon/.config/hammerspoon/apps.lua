38fbdf9af529760ccdc0d0a37ae46d45b405350f
giogt <giogt.dev@pm.me>
2026-04-23 04:54:30 +0100

    [hammerspoon] Improve window management

---
+++ hammerspoon/.config/hammerspoon/apps.lua
@@ -0,0 +1,25 @@
+local _apps = {
+	brave_browser = {
+		name = "Brave Browser",
+		switchKey = "K",
+		dimensions = { w = 1800, h = 1200 },
+		offset = { x = 0, y = 0 },
+	},
+	ghostty = {
+		name = "Ghostty",
+		switchKey = "J",
+		dimensions = { w = 1392, h = 1080 },
+	},
+	neovide = {
+		name = "Neovide",
+		switchKey = "L",
+		dimensions = { w = 1600, h = 1150 },
+	},
+	proton_mail = {
+		name = "Proton Mail",
+		switchKey = "U",
+		dimensions = { w = 1800, h = 1200 },
+	},
+}
+
+return _apps
--- hammerspoon/.config/hammerspoon/windows.lua
+++ hammerspoon/.config/hammerspoon/windows.lua
@@ -1,7 +1,106 @@
 local hs = hs
 
+-- ------------------------------------
+-- Apps
+-- ------------------------------------
+
+local _apps = require("apps")
+local _appsByName = {}
+for _, app in pairs(_apps) do
+	_appsByName[app.name] = app
+end
+
+-- ------------------------------------
+-- Config
+-- ------------------------------------
+
 -- Disable window animations
 hs.window.animationDuration = 0
+
+-- Start and use isolate mode
+-- hs.window.highlight.ui.overlay = true
+-- hs.window.highlight.start()
+-- hs.window.highlight.toggleIsolate()
+
+-- ------------------------------------
+-- Utility functions
+-- ------------------------------------
+
+local addWindowBinding = function(mod, key, func)
+	hs.hotkey.bind(mod, key, function()
+		local win = hs.window.focusedWindow()
+		if not win then
+			return
+		end
+		func(win)
+	end)
+end
+
+local setWindowFrame = function(win, dimensions, offset)
+	offset = offset or { x = 0, y = 0 }
+
+	local screen = win:screen():frame()
+
+	local x = screen.x + (screen.w - dimensions.w) / 2
+	local y = screen.y + (screen.h - dimensions.h) / 2
+
+	x = math.max(x, screen.x)
+	y = math.max(y, screen.y)
+
+	if y > 100 then
+		y = 100
+	end
+
+	win:setFrame({ x = x + offset.x, y = y + offset.y, w = dimensions.w, h = dimensions.h })
+	win:focus()
+end
+
+local hideOtherApps = function()
+	local frontApp = hs.application.frontmostApplication()
+	for _, app in ipairs(hs.application.runningApplications()) do
+		if app ~= frontApp and app:isRunning() and not app:isHidden() then
+			app:hide()
+		end
+	end
+end
+
+-- Switch to the app with appName, only if the app has a window in the current
+-- desktop. If the app is not running, launch it.
+local switchToAppInCurrentDesktop = function(appName)
+	local app = hs.application.get(appName)
+	if not app then
+		hs.application.open(appName)
+		return
+	end
+
+	local currentSpace = hs.spaces.focusedSpace()
+	for _, win in ipairs(app:allWindows()) do
+		local spaces = hs.spaces.windowSpaces(win)
+		if spaces then
+			for _, space in ipairs(spaces) do
+				if space == currentSpace then
+					win:focus()
+					hideOtherApps()
+					return
+				end
+			end
+		end
+	end
+end
+
+-- ------------------------------------
+-- Switch windows
+-- ------------------------------------
+
+for _, app in pairs(_apps) do
+	if not app.switchKey then
+		return
+	end
+
+	hs.hotkey.bind({ "alt" }, app.switchKey, function()
+		switchToAppInCurrentDesktop(app.name)
+	end)
+end
 
 -- ------------------------------------
 -- Move window
@@ -11,26 +110,25 @@ local moveWinMod = { "ctrl", "alt", "cmd" }
 local moveIncrX = 50
 local moveIncrY = 35
 
-hs.hotkey.bind(moveWinMod, "Left", function()
-	local win = hs.window.focusedWindow()
+addWindowBinding(moveWinMod, "Left", function(win)
 	local frame = win:frame()
 	frame.x = frame.x - moveIncrX
 	win:setFrame(frame)
 end)
-hs.hotkey.bind(moveWinMod, "Right", function()
-	local win = hs.window.focusedWindow()
+
+addWindowBinding(moveWinMod, "Right", function(win)
 	local frame = win:frame()
 	frame.x = frame.x + moveIncrX
 	win:setFrame(frame)
 end)
-hs.hotkey.bind(moveWinMod, "Up", function()
-	local win = hs.window.focusedWindow()
+
+addWindowBinding(moveWinMod, "Up", function(win)
 	local frame = win:frame()
 	frame.y = frame.y - moveIncrY
 	win:setFrame(frame)
 end)
-hs.hotkey.bind(moveWinMod, "Down", function()
-	local win = hs.window.focusedWindow()
+
+addWindowBinding(moveWinMod, "Down", function(win)
 	local frame = win:frame()
 	frame.y = frame.y + moveIncrY
 	win:setFrame(frame)
@@ -44,29 +142,25 @@ local snapWinMod = { "ctrl", "alt" }
 local snapMarginX = 5
 local snapMarginY = 5
 
-hs.hotkey.bind(snapWinMod, "Left", function()
-	local win = hs.window.focusedWindow()
+addWindowBinding(snapWinMod, "Left", function(win)
 	local winFrame = win:frame()
 	local screenFrame = win:screen():frame()
 	winFrame.x = screenFrame.x + snapMarginX
 	win:setFrame(winFrame)
 end)
-hs.hotkey.bind(snapWinMod, "Right", function()
-	local win = hs.window.focusedWindow()
+addWindowBinding(snapWinMod, "Right", function(win)
 	local winFrame = win:frame()
 	local screenFrame = win:screen():frame()
 	winFrame.x = screenFrame.w - winFrame.w - snapMarginX
 	win:setFrame(winFrame)
 end)
-hs.hotkey.bind(snapWinMod, "Up", function()
-	local win = hs.window.focusedWindow()
+addWindowBinding(snapWinMod, "Up", function(win)
 	local winFrame = win:frame()
 	local screenFrame = win:screen():frame()
 	winFrame.y = screenFrame.y + snapMarginY
 	win:setFrame(winFrame)
 end)
-hs.hotkey.bind(snapWinMod, "Down", function()
-	local win = hs.window.focusedWindow()
+addWindowBinding(snapWinMod, "Down", function(win)
 	local winFrame = win:frame()
 	local screenFrame = win:screen():frame()
 	winFrame.y = screenFrame.h - winFrame.h - snapMarginY
@@ -74,42 +168,40 @@ hs.hotkey.bind(snapWinMod, "Down", function()
 end)
 
 -- ------------------------------------
--- Resize window
+-- Set window dimensions
 -- ------------------------------------
 
-local resizeWinMod = { "ctrl", "alt" }
-hs.hotkey.bind(resizeWinMod, "D", function()
+hs.hotkey.bind({ "ctrl", "alt" }, "D", function()
 	local win = hs.window.focusedWindow()
 	if not win then
 		return
 	end
 
-	local button, input = hs.dialog.textPrompt("Resize Window", "Enter dimensions (WxH):", "1600x1150", "OK", "Cancel")
+	local button, input =
+		hs.dialog.textPrompt("Window dimensions", "Enter dimensions (WxH):", "1600x1150", "OK", "Cancel")
 
 	if button == "OK" then
 		local w, h = input:match("(%d+)x(%d+)")
 		if w and h then
 			w, h = tonumber(w), tonumber(h)
 
-			local screen = win:screen():frame()
-
-			-- Calculate window position
-			local x = screen.x + (screen.w - w) / 2 -- horizontally centered
-			local y = screen.y + (screen.h - h) / 2 -- vertically centered
-
-			-- Clamp to screen bounds in case the window is larger than the screen
-			x = math.max(x, screen.x)
-			y = math.max(y, screen.y)
-
-			-- Prefer placing the window in the top area of the screen
-			if y > 100 then
-				y = 100
-			end
-
-			win:setFrame({ x = x, y = y, w = w, h = h })
-			win:focus()
+			setWindowFrame(win, { w = w, h = h })
 		else
 			hs.alert.show("Invalid format. Use <width>x<height> (e.g. 800x600)")
 		end
 	end
+end)
+
+-- -----------------------------------------------------------------------------
+-- Set default size and position for the current window
+-- -----------------------------------------------------------------------------
+
+addWindowBinding({ "alt" }, "S", function(win)
+	local appName = win:application():name()
+	local appConfig = _appsByName[appName]
+	if not appConfig then
+		return
+	end
+
+	setWindowFrame(win, appConfig.dimensions, appConfig.offset)
 end)
