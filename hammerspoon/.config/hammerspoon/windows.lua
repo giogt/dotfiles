05ea2e156b04b6e52007c7fce99f2e41f44c5cc4
giogt <giogt.dev@pm.me>
2026-04-24 16:15:12 +0100

    [hammerspoon] Improve window management

--- hammerspoon/.config/hammerspoon/windows.lua
+++ hammerspoon/.config/hammerspoon/windows.lua
@@ -26,7 +26,7 @@ hs.window.animationDuration = 0
 -- Utility functions
 -- ------------------------------------
 
-local addWindowBinding = function(mod, key, func)
+local addBindingToFocusedWindow = function(mod, key, func)
 	hs.hotkey.bind(mod, key, function()
 		local win = hs.window.focusedWindow()
 		if win ~= nil then
@@ -35,69 +35,16 @@ local addWindowBinding = function(mod, key, func)
 	end)
 end
 
-local setWindowFrame = function(win, dimensions, offset)
-	offset = offset or { x = 0, y = 0 }
-
-	local screen = win:screen():frame()
-
-	local x = screen.x + (screen.w - dimensions.w) / 2
-	local y = screen.y + (screen.h - dimensions.h) / 2
-
-	x = math.max(x, screen.x)
-	y = math.max(y, screen.y)
-
-	if y > 100 then
-		y = 100
-	end
-
-	win:setFrame({ x = x + offset.x, y = y + offset.y, w = dimensions.w, h = dimensions.h })
-	win:focus()
-end
-
-local hideOtherApps = function()
-	local frontApp = hs.application.frontmostApplication()
-	for _, app in ipairs(hs.application.runningApplications()) do
-		if app ~= frontApp and app:isRunning() and not app:isHidden() then
-			app:hide()
-		end
-	end
-end
-
--- Switch to the app with appName
-local switchToApp = function(appName)
-	-- In Desktop & Dock settings, I keep the following toggle disabled: "When
-	-- switching to an application, switch to a Space with open windows for the
-	-- current application".
-	--
-	-- This behaviour works as intended on MacOS when switching to apps that are
-	-- not hidden. When an app is hidden, switching to it automatically unhides
-	-- the app and, if the current Space does not have any window for the app, it
-	-- automatically switches to a Space which does, even if the setting mentioned
-	-- above is disabled.
-	--
-	-- This is just how MacOS works and, as far as I know, there isn't a way to
-	-- change this behaviour.
-	--
-	-- The code below is a workaround. If the app we are switching to is hidden,
-	-- then we are unhiding it first without activating it, and sleep for a
-	-- little to make sure the app is fully unhidden before executing the next
-	-- steps.
-	local app = hs.application.get(appName)
-	if app ~= nil then
-		if app:isHidden() then
-			app:unhide()
-			hs.timer.usleep(10000)
-		end
-	end
-
-	hs.application.open(appName)
-	hideOtherApps()
-end
-
 -- ------------------------------------
 -- Switch apps
 -- ------------------------------------
 
+-- Switch to the app with appName
+local switchToApp = function(appName)
+	hs.application.open(appName)
+end
+
+-- Register key bindings to switch to apps
 for _, app in pairs(_apps) do
 	if app.switchKey then
 		hs.hotkey.bind({ "alt" }, app.switchKey, function()
@@ -107,64 +54,64 @@ for _, app in pairs(_apps) do
 end
 
 -- ------------------------------------
--- Move window
+-- Move windows
 -- ------------------------------------
 
 local moveWinMod = { "ctrl", "alt", "cmd" }
 local moveIncrX = 50
 local moveIncrY = 35
 
-addWindowBinding(moveWinMod, "Left", function(win)
+addBindingToFocusedWindow(moveWinMod, "Left", function(win)
 	local frame = win:frame()
 	frame.x = frame.x - moveIncrX
 	win:setFrame(frame)
 end)
 
-addWindowBinding(moveWinMod, "Right", function(win)
+addBindingToFocusedWindow(moveWinMod, "Right", function(win)
 	local frame = win:frame()
 	frame.x = frame.x + moveIncrX
 	win:setFrame(frame)
 end)
 
-addWindowBinding(moveWinMod, "Up", function(win)
+addBindingToFocusedWindow(moveWinMod, "Up", function(win)
 	local frame = win:frame()
 	frame.y = frame.y - moveIncrY
 	win:setFrame(frame)
 end)
 
-addWindowBinding(moveWinMod, "Down", function(win)
+addBindingToFocusedWindow(moveWinMod, "Down", function(win)
 	local frame = win:frame()
 	frame.y = frame.y + moveIncrY
 	win:setFrame(frame)
 end)
 
 -- ------------------------------------
--- Snap window
+-- Snap windows
 -- ------------------------------------
 
 local snapWinMod = { "ctrl", "alt" }
 local snapMarginX = 5
 local snapMarginY = 5
 
-addWindowBinding(snapWinMod, "Left", function(win)
+addBindingToFocusedWindow(snapWinMod, "Left", function(win)
 	local winFrame = win:frame()
 	local screenFrame = win:screen():frame()
 	winFrame.x = screenFrame.x + snapMarginX
 	win:setFrame(winFrame)
 end)
-addWindowBinding(snapWinMod, "Right", function(win)
+addBindingToFocusedWindow(snapWinMod, "Right", function(win)
 	local winFrame = win:frame()
 	local screenFrame = win:screen():frame()
 	winFrame.x = screenFrame.w - winFrame.w - snapMarginX
 	win:setFrame(winFrame)
 end)
-addWindowBinding(snapWinMod, "Up", function(win)
+addBindingToFocusedWindow(snapWinMod, "Up", function(win)
 	local winFrame = win:frame()
 	local screenFrame = win:screen():frame()
 	winFrame.y = screenFrame.y + snapMarginY
 	win:setFrame(winFrame)
 end)
-addWindowBinding(snapWinMod, "Down", function(win)
+addBindingToFocusedWindow(snapWinMod, "Down", function(win)
 	local winFrame = win:frame()
 	local screenFrame = win:screen():frame()
 	winFrame.y = screenFrame.h - winFrame.h - snapMarginY
@@ -172,10 +119,31 @@ addWindowBinding(snapWinMod, "Down", function(win)
 end)
 
 -- ------------------------------------
--- Set window dimensions
+-- Window size and dimension
 -- ------------------------------------
 
-addWindowBinding({ "ctrl", "alt" }, "D", function(win)
+local setWindowFrame = function(win, dimensions, offset)
+	offset = offset or { x = 0, y = 0 }
+
+	local screen = win:screen():frame()
+
+	local topMargin = 5
+	local verticalShift = dimensions.h * 0.05 + 20
+
+	local x = screen.x + (screen.w - dimensions.w) / 2
+	local y = screen.y + (screen.h - dimensions.h) / 2 - verticalShift
+
+	x = math.max(x, screen.x)
+	y = math.max(y, screen.y + topMargin)
+	if dimensions.h >= screen.h - topMargin then
+		y = screen.y
+	end
+
+	win:setFrame({ x = x + offset.x, y = y + offset.y, w = dimensions.w, h = dimensions.h })
+	win:focus()
+end
+
+addBindingToFocusedWindow({ "ctrl", "alt" }, "D", function(win)
 	local button, input =
 		hs.dialog.textPrompt("Window dimensions", "Enter dimensions (WxH):", "1600x1150", "OK", "Cancel")
 
@@ -195,7 +163,7 @@ end)
 -- Set default size and position for the current window
 -- -----------------------------------------------------------------------------
 
-addWindowBinding({ "alt" }, "S", function(win)
+addBindingToFocusedWindow({ "alt" }, "S", function(win)
 	local appName = win:application():name()
 	local appConfig = _appsByName[appName]
 	if appConfig ~= nil then
