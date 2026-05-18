a21b858aa7561ed3e0441727b797fea1e6e537b4
giogt <giogt.dev@pm.me>
2026-05-18 01:32:47 +0100

    [linux] Update Fedora init script

--- linux/.config/fedora/init.sh
+++ linux/.config/fedora/init.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
+# NOTE: Do not execute this script directly. Execute the steps below manually
+
 cd ~
 
 # install base packages
 sudo dnf install -y fzf git nvim stow which zoxide zsh
+
+# clone my dotfiles config
+git clone https://github.com/giogt/dotfiles.git ~/.dotfiles
+
+# install terminfo for the Ghostty terminal emulator
+tic -x ~/.dotfiles/shell/.config/terminfo/ghostty.terminfo
+
+# change shell to zsh
+sudo chsh -s /usr/bin/zsh giogt
+
+# NOTE: if using Lima VM, restart before next steps
 
 # install zimfw
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
@@ -15,14 +28,9 @@ curl https://mise.run | sh
 mv ~/.zshrc ~/.zshrc.bak 
 mv ~/.zimrc ~/.zimrc.bak
 
-# clone my dotfiles config and apply it
-git clone https://github.com/giogt/dotfiles.git ~/.dotfiles
+# NOTE: add ~/.config/mise/github_tokens.toml before next steps to avoid Github rate limit with Mise
+
 stow -d ~/.dotfiles shell
-
-# install terminfo for the Ghostty terminal emulator
-tic -x ~/.config/terminfo/ghostty.terminfo
-# tic -x ~/.dotfiles/shell/.config/terminfo/ghostty.terminfo
-
-# change shell to zsh
-sudo chsh -s /usr/bin/zsh giogt
+stow -d ~/.dotfiles linux
+stow -d ~/.dotfiles dev
 
